d2a53d83246c1bfd7a1cdd1cf83a53ac9ddde017
kompowiec2 <diasp@o2.pl>
2023-05-10 23:21:52 +0200

    Update evil.el
    toggle

--- .emacs.d/evil.el
+++ .emacs.d/evil.el
@@ -1,4 +1,19 @@
 (require 'evil)
-(evil-mode 1)
+(defun toggle-evil-mode ()
+	"Toggle evil mode."
+	(interactive)
+	(if (bound-and-true-p evil-mode)
+		(evil-mode 0)
+		(evil-mode 1)))
+(defun add-toggle-evil-mode-to-options-menu ()
+	"Add toggle evil mode to options menu."
+	(define-key menu-bar-options-menu [toggle-evil-mode]
+							`(menu-item ,(if (bound-and-true-p evil-mode)
+														 "Disable Evil Mode"
+														 "Enable Evil Mode")
+													toggle-evil-mode
+													:help ,(if (bound-and-true-p evil-mode)
+																	 "Turn off Evil Mode"
+																	 "Turn on Evil Mode"))))
+(add-toggle-evil-mode-to-options-menu)
 (setq evil-want-C-i-jump nil) ;Tab functionality back
-

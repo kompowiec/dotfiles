bd0f01b8aca65969c4dcaffdbba1a38f6665a37f
kompowiec2 <diasp@o2.pl>
2024-03-14 18:34:20 +0100

    refresh ELPA

--- .emacs.d/package-manager.el
+++ .emacs.d/package-manager.el
@@ -1,3 +1,13 @@
+;; https://github.com/jwiegley/use-package
+(eval-when-compile
+  (require 'use-package))
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))

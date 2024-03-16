5683e91a6d73bae818efc13c383aa593ae6f66dd
kompowiec2 <diasp@o2.pl>
2024-03-16 11:09:55 +0100

    eLPA

--- .emacs.d/package-manager.el
+++ .emacs.d/package-manager.el
@@ -1,13 +1,17 @@
 ;; https://github.com/jwiegley/use-package
 (eval-when-compile
-  (require 'use-package))
+	(require 'use-package))
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+						 '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+(add-to-list 'package-archives
+						 (cons "nongnu" (format "http%s://elpa.nongnu.org/nongnu/"
+																		(if (gnutls-available-p) "s" ""))))

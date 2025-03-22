3725ea085061c34c0ddbcad578e6746384ce214a
kompowiec2 <diasp@o2.pl>
2025-03-22 09:18:37 +0100

    android look
    Easier navigation

--- .emacs.d/termux.el
+++ .emacs.d/termux.el
@@ -1,3 +1,10 @@
+;;android look
+(global-visual-line-mode 1)  ;; Enable word wrapping
+(setq pixel-scroll-precision-use-momentum nil) ;; Disable momentum scrolling  
+(pixel-scroll-precision-mode 1)  ;; Smooth scrolling  
+(setq scroll-conservatively 101) ;; Prevent sudden jumps
+
+;;termux shell
 (setenv "SHELL" "/data/data/com.termux/files/usr/bin/bash")
 (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
                (getenv "PATH")))
